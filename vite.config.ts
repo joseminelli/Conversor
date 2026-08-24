9387ac69c54f4ec385ae6beb689dff5d0c950cc4
Jose Minelli <zecarminelli@gmail.com>
2026-08-24 09:49:11 -0300

    fix: Remove GITHUB_PAGES condition from base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  base: process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES === 'true' ? '/Conversor/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Conversor/' : '/',
   server: {
     port: 5173,
     host: true
